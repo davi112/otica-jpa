36f8c7cc373ced11a8a07e2e21226b79facc52dd
Davi Fonseca <davimartinsfreitas@gmail.com>
2023-11-19 22:23:02 -0300

    mudanca em configuracoes necessarias

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ import org.example.controllers.cadastro.LoginController;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         LoginController loginController = new LoginController();
     }
 }
